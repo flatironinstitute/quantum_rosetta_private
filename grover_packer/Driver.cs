92992ee3f9b44c2e462db59d444d418382a400c1
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2018-09-20 12:35:14 -0400

    Working on importing data.

--- grover_packer/Driver.cs
+++ grover_packer/Driver.cs
@@ -6,11 +6,46 @@ using System.Collections.Generic;
 
 namespace grover_packer
 {
+    /// @brief A class for information about an individual rotamer.
+    public class Rotamer
+    {
+        /// @brief The sequence position of this rotamer.
+        public int SeqPos { get; set; }
+
+        /// @brief The index of the rotamer at this sequence position.
+        public int RotIndex { get; set; }
+
+        /// @brief The onebody energy of this rotamer.
+        /// @details Includes interactions with background.
+        public double OneBodyEnergy { get; set; }
+    }
+
     class Driver
     {
+        /// @brief The list of rotamers, indexed as (seqpos, rotindex).
+        private List< List< Rotamer > > rotamer_list_;
+
+        private void parse_rotamers_and_onebody_energies( List<string> all_lines ) {
+            bool in_field = false;
+            bool breaktime = false;
+            for( int i=1; i<=all_lines.Count; ++i ) {
+                if( in_field ) {
+                    //TODO
+                } else {
+                    if( all_lines[i] == "[BEGIN ONEBODY SEQPOS/ROTINDEX/ENERGY]\n" ) {
+                        in_field = true;
+                    }
+                    continue;
+                }
+                if( breaktime ) break;
+            }
+        }
+
         /// @brief Parse an array of strings and set up variables describing this packer problem.
         void do_parse( List< string > all_lines ) {
-            //TODO
+            rotamer_list_ = new List< List < Rotamer > >();
+            parse_rotamers_and_onebody_energies( all_lines );
+            //parse_twobody_energies( all_lines );
         }
 
         /// @brief Slurp the contents of a packer description file into an array of strings.
